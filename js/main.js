cf16c9a5de21c8c6acd227ae00f1092acd187ad8
ratimort <31414746+ratimort@users.noreply.github.com>
2020-04-14 07:25:33 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+  $(".click-on").click(function(){
+    $(".all").toggleClass("all-1");
+    $(".all-bl").fadeToggle(100)
+    $(".toggle-menu").toggleClass("right-toggle")
+  });
+});
+
+
+
+
